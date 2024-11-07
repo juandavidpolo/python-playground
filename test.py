b1a5b25d93fccb935dc6fbfb6e7237f13c252784
Juan David Polo <juandavidpolomendez@gmail.com>
2024-11-07 17:18:04 -0500

    refactor: fixing code

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 """Functions for test github actions"""
 
-def add (a:int, b:int) -> int:
+
+def add(a: int, b: int) -> int:
     """Addition of two numbers
     """
 
     return a + b
 
-def sustraction (a:int, b:int) -> int:
+
+def sustraction(a: int, b: int) -> int:
     """Sustraction of two numbers
     """
 
     return a - b
 
-def divide (a:int, b:int) ->int:
+
+def divide(a: int, b: int) ->int:
     return a/b
 
-def square(a:int) ->int:
+
+def square(a: int) ->int:
     return a**2
 
-def root (a:int, b:int) ->float:
+
+def root(a: int, b: int) ->float:
     return a**(1/b)
