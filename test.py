292864e5e7eabe6909ee3db1852b21608550e19c
Juan David Polo <juandavidpolomendez@gmail.com>
2024-11-07 15:12:58 -0500

    feat: pipeline for code quality

---
+++ test.py
@@ -0,0 +1,13 @@
+"""Functions for test github actions"""
+
+def add (a:int, b:int) -> int:
+    """Addition of two numbers
+    """
+
+    return a + b
+
+def sustraction (a:int, b:int) -> int:
+    """Sustraction of two numbers
+    """
+
+    return a - b
